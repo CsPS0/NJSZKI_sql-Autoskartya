e16b73ec564fa8eca5751054d02527edfe547a73
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2024-09-22 10:58:13 +0200

    Fixed Exercises
    same

--- Solti-CsongorPeter-autoskartya1.sql
+++ Solti-CsongorPeter-autoskartya1.sql
@@ -1,16 +1,16 @@
 -- 3. feladat
 SELECT * FROM `autoskartya`;
 -- 4. feladat
-SELECT `gyartmany`, `tipus` FROM `autoskartya` WHERE orszagkod = `D` AND vegsebesseg > 240 OR loero >= 200; 
+SELECT `gyartmany`, `tipus` FROM `autoskartya` WHERE orszagkod = `D` AND (vegsebesseg > 240 OR loero >= 200) ORDER BY `tipus` DESC; 
 -- 5. feladat
-SELECT * FROM `autoskartya` WHERE orszagkod = `J` AND fogyasztas <= 6 OR hengerurtartalom <= 1400 fogyasztas <= 7;
+SELECT * FROM `autoskartya` WHERE orszagkod = `J` AND (fogyasztas <= 6 OR (hengerurtartalom <= 1400 fogyasztas <= 7));
 -- 6. feladat
-SELECT `gyartmany`, `tipus`, `gyartasi_ev` FROM `autoskartya` WHERE gyartmany = `Bentley`, `BMW`, `Mercedes`, `Volvo` AND teljesitmenye >= 150;
+SELECT `gyartmany`, `tipus`, `gyartasi_ev` FROM `autoskartya` WHERE gyartmany IN (`Bentley`, `BMW`, `Mercedes`, `Volvo`) AND kilowatt >= 150;
 -- 7. feladat
-SELECT `gyartmany` FROM `autoskartya` ORDER BY ASC;
+SELECT DISTINCT `gyartmany` FROM `autoskartya` ORDER BY `gyartmany` ASC;
 -- 8. feladat
-SELECT `gyartmany`, `tipus`, `loero` FROM `autoskartya` ORDER BY loero ASC;
+SELECT `gyartmany`, `tipus`, `loero` FROM `autoskartya` ORDER BY `loero` ASC;
 -- 9. feladat
-SELECT `tipus`, `orszag`, `vegsebesseg` FROM `autoskartya` ORDER BY orszag = (NO REPEAT);
+SELECT `tipus`, `orszag`, `vegsebesseg` FROM `autoskartya` GROUP BY `orszag`, `tipus`, `vegsebesseg` ORDER BY `orszag` ASC;
 -- 10. feladat
-SELECT `gyartmany`, `tipus`, `orszagkod` FROM `autoskartya` WHERE orszagkod = `D` AND orszagkod = `F` AND fogyasztas > 5;
+SELECT `gyartmany`, `tipus`, `orszagkod` FROM `autoskartya` WHERE orszagkod IN (`D`, `F`) AND fogyasztas < 5;
